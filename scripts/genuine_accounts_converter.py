ebce8393af942e0d661ead43a14b0a7d2a03a79a
Patryk Sulej <p.sulej@prometheus-sa.com>
2022-11-17 23:57:54 +0100

    scripts for mib

--- scripts/genuine_accounts_converter.py
+++ scripts/genuine_accounts_converter.py
@@ -19,21 +19,31 @@ def generate_nodes_to_graph_id(graph_labels_npy_path, node_graph_id_npy_path, ou
 headerList = ['id','text','source','user_id','truncated','in_reply_to_status_id','in_reply_to_user_id','in_reply_to_screen_name','retweeted_status_id','geo','place','contributors','retweet_count','reply_count','favorite_count','favorited','retweeted','possibly_sensitive','num_hashtags','num_urls','num_mentions','created_at','timestamp','crawled_at','updated']
 
 
-with open('./resources/genuine_accounts.csv/tweets.csv', 'r', encoding='utf-8') as in_file, open('./resources/genuine_accounts.csv/tweets_demo2.csv', 'w+', encoding='utf-8', newline='') as out_file:
+def extract_url(input):
+    pattern = "((https?):((//))+([\w\d:#@%/;$()~_?\+-=\.&](#!)?)*)"
+    match = re.search(pattern, input)
+    return match.group(1) if match else ''
+
+with open('./resources/genuine_accounts.csv/tweets.csv', 'r', encoding='utf-8') as in_file, open('./resources/genuine_accounts.csv/tweets_processed.csv', 'w+', encoding='utf-8', newline='') as out_file:
+
     stop = None
     writer = csv.writer(out_file)
     writer.writerow(headerList)
     idx = 0
-    for i, line in enumerate(csv.reader(in_file)):
+    for i, line in enumerate(csv.reader(
+        in_file,
+        delimiter=',',
+        quotechar='"',
+        quoting=csv.QUOTE_ALL,
+        skipinitialspace=True,
+        escapechar='\\')):
+
         if stop and idx == stop:
             break
         
+        # skip broken text
         line_len = len(line)
-        if (line_len == 27):
-            line[1] = line[1] + line[2]
-            del line[2]
-            line_len = len(line)
-        elif line_len != 26:
+        if line_len != 26:
             continue
 
         for index, cell in enumerate(line):
@@ -43,10 +53,18 @@ with open('./resources/genuine_accounts.csv/tweets.csv', 'r', encoding='utf-8') 
         # merge geolocation coords
         line[9] = line[9] + (':' if line[9] and line[10] else '') + line[10]
         del line[10]
+
+        # extract url
+        line[2] = extract_url(line[2])
+
+        for pos, entry in enumerate(line):
+            line[pos] = re.sub('"|\"', "'", entry)
+            line[pos] = line[pos].replace(",", "")
+            line[pos] = line[pos].replace("\\", "\\_")
         
         writer.writerow(line)
         idx += 1
-        if i%100000 == 0:
+        if i%10000 == 0:
             print(int(i/8470000*100), '%')
     print('Done')
 
